97dc66ebb8198e20b2e42bf590a87b316a605fa1
Jinghao Hu <731409213@qq.com>
2026-08-12 05:27:30 +0800

    Show 32 top followers in profile README

--- src/getTopFollowers.py
+++ src/getTopFollowers.py
@@ -87,7 +87,7 @@ query {{
 
     html = "<table>\n"
 
-    for i in range(min(len(followers), 24)):
+    for i in range(min(len(followers), 32)):
         login = followers[i][1]
         id = followers[i][2]
         name = followers[i][3]
